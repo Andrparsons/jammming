4a0bda4a3b3b944e34dafb04e0277e1f21e50e35
andrparsons <andrparsons@gmail.com>
2017-11-20 12:53:08 -0500

    Added a song preview button to the search results

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './Track.css';
+import React from "react";
+import "./Track.css";
 
 class Track extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
@@ -17,23 +17,50 @@ class Track extends React.Component {
   }
 
   renderAction() {
-    if(this.props.isRemoval) {
-      return <a className="Track-action" onClick={this.removeTrack} >-</a>;
+    if (this.props.isRemoval) {
+      return (
+        <a className="Track-action" onClick={this.removeTrack}>-</a>
+      );
     } else {
-      return <a className="Track-action" onClick={this.addTrack} >+</a>;
+      return (
+        <div>
+          <a className="Track-action" onClick={this.addTrack}>+</a>
+        </div>
+      );
     }
+  }
+
+  renderPreview() {
+    if (!this.props.isRemoval) {
+      return (
+        <div className="embed-container">
+          <iframe
+            title={this.props.track.name}
+            src={`https://open.spotify.com/embed?uri=spotify:track:${this.props.track.id}`}
+            frameBorder="0"
+            allowTransparency="true"
+          />
+        </div>
+      )
+    } else {
+      return (
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}
+          </p>
+        </div>
+      )
+    } 
   }
 
   render() {
     return (
       <div className="Track">
-        <div className="Track-information">
-          <h3>{this.props.track.name}</h3>
-          <p>{this.props.track.artist} | {this.props.track.album}</p>
-        </div>
+        {this.renderPreview()}
         {this.renderAction()}
       </div>
-    )
+    );
   }
 }
 
